f107a9562ef4a0c53d4f2a818a6ff21382fa2d4a
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-02-23 23:27:20 +0530

    heroSectionBtn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] ">
-        <h1 className="text-2xl flex justify-center items-center mt-10">Hello Jee Home page hai</h1>
+        {/* <h1 className="text-2xl flex justify-center items-center mt-10">Hello Jee Home page hai</h1> */}
+        <HeroSection />
     </main>
   );
 }
