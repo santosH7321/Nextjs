8de809e85a310835e5c7afded1557f9353357ffa
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-02-23 23:47:58 +0530

    fetureSection

---
+++ src/components/FeaturedCourses.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+import courseData from "../data/music_courses.json"
+const FeaturedCourses = () => {
+    
+  return (
+    <div className="py-12 bg-gray-900">
+      <div>
+        <div className="text-center">
+            <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">FEATURED COURSES</h2>
+            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Learn With the Best</p>
+        </div>
+      </div>
+      <div className="mt-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+            Santosh
+        </div>
+      </div>
+      <div className="mt-20 text-center">
+        <Link href={"/courses"} 
+        className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
+        >
+            View All courses
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default FeaturedCourses
