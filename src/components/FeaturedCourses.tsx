a919bd1d8f74ea9dd9a4b178fa302158e1c677f7
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-02-24 00:23:50 +0530

    grid

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,7 +1,21 @@
+'use client'
 import Link from "next/link"
 import courseData from "../data/music_courses.json"
+import { BackgroundGradient } from "./ui/background-gradient"
+
+interface Course{
+    id: number,
+    title: string,
+    slug: string,
+    description: string,
+    price: number,
+    instructor: string,
+    isFeatured: boolean,
+    // image: "/courses/guitar.jpg" 
+}
 const FeaturedCourses = () => {
-    
+    const featuredCourses = courseData.courses.filter((course:Course) => course.isFeatured)
+
   return (
     <div className="py-12 bg-gray-900">
       <div>
@@ -12,7 +26,23 @@ const FeaturedCourses = () => {
       </div>
       <div className="mt-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
-            Santosh
+            {featuredCourses.map((course:Course) => (
+                <div key={course.id} className="flex justify-center">
+                    <BackgroundGradient
+                     className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm"
+                    >
+                        <div
+                        className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow" 
+                        >
+                            <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
+                            <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
+                            <Link href={`/courses/${course.slug}`}>
+                                Learn More
+                            </Link>
+                        </div>
+                    </BackgroundGradient>
+                </div>
+            ))}
         </div>
       </div>
       <div className="mt-20 text-center">
