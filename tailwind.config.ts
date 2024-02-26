802f0072c8c81ef466226f7454e30fc1a44bd13d
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-02-26 16:25:42 +0530

    infiniteMovingCard

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,7 @@ const config: Config = {
   theme: {
     extend: {
       animation: {
+        scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
         spotlight: "spotlight 2s ease .75s 1 forwards",
       },
       keyframes: {
@@ -40,6 +41,11 @@ const config: Config = {
             transform: "translate(-50%,-40%) scale(1)",
           },
         },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -50,4 +56,5 @@ const config: Config = {
   },
   plugins: [addVariablesForColors],
 };
+
 export default config;
